b7d24f9ce6c07c8cd4c9fcfa3674908484f6f55e
jjheipz24 <heipler.jj@gmail.com>
2018-12-01 20:56:30 -0500

    trash icon

--- app.js
+++ app.js
@@ -4,3 +4,26 @@
  base.scaleToWindow();
 
  base.start();
+
+ function load() {
+
+ }
+
+ function setup() {
+
+     //green panel
+     let backBox = base.rectangle(
+         150, base.canvas.height, 0x017301, 0x017301, 1, 0, 0);
+     //trash icon
+     let trash = base.sprite("trashbag.svg");
+     trash.anchor.x = 0.5;
+     trash.anchor.y = 0.5;
+     backBox.putTop(trash, 0, -trash.y);
+     trash.scale.set(0.5, 0.5);
+     let trashText = base.text("Trash", "12px Nunito", "white");
+     trash.putCenter(trashText, 0, 20);
+ }
+
+ function play() {
+
+ }
