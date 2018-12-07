72f77e90f0f9e676aa25e4611a837c47857c731a
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2018-12-07 14:39:50 -0500

    attempts

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function loadProgram() {
     let base = hexi(800, 400, setup);
     base.border = "6px #003449 solid";
     base.backgroundColor = "0x007bb8";
-    base.scaleToWindow();
+    //base.scaleToWindow();
 
     base.start();
 
